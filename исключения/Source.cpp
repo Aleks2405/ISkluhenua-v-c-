9a0aa471239424412c2fd0b020b547f091ab1877
Goro_sb71@stud.msk.itstep.org <Goro_sb71@stud.msk.itstep.org>
2021-12-10 17:17:04 +0300

    добавили класс но увы не вышло нечего

--- исключения/Source.cpp
+++ исключения/Source.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+  #include <iostream>
+#include <fstream>
 using namespace std;
 /*Задание 2.
 Создайте иерархию пользовательских классов-исключений.
@@ -16,13 +17,12 @@ public:
 		
 	 int as (){
 		int z = one * two;
-		if (one > 0 && two < 0 || one < 0 && two > 0) {
-			throw - z;
-		}
-		else {
-			return z;
-		}
-		
+			if (one > 0 && two < 0 || one < 0 && two > 0) {
+				throw - z;
+			}
+			else {
+				return z;
+			}
 	}
 	friend ostream& operator << (ostream& out, const YM& p) {
 		out << p.one << " " << p.two << endl;
@@ -35,24 +35,60 @@ public:
 	}
 };
 class rabot {
-	
 public:
-	
+	void file(const char* part) {
+		ofstream fout(part);
+		if (!fout)
+			throw "File not opened!";
+		
+	}
 };
 int main() {
-	YM one(-5, 5);
-	try {
-		one.as();
-	}
-	catch (int) {
-		cout << "You entered the wrong number" << endl;
-		cin >> one;
-	}
+	//YM one(-5, 5);
+	//try {
+	//	one.as();
+	//}
+	//catch (int) {
+	//		cout << "You entered the wrong number" << endl;
+	//		cin >> one;
+	//	}
 	
-	one.as();
-	cout << one.as() << endl;
-	cout << one <<endl;
+	//one.as();
+	//cout << one.as() << endl;
+	rabot one;
+	try {
+		one.file("arr.txt");
+	}
+	catch (char*err) {
+		cout << " fail netu" << endl;
+	}
+
 
 	cout << "proga otrabotala" << endl;
 	
 }
+/*
+#include <iostream>
+#include <exception>
+
+class A
+{
+public:
+	void foo()
+	{
+		throw std::exception("Oops!");
+	}
+};
+
+int main()
+{
+	try
+	{
+		A a;
+		a.foo();
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << "tut" << std::endl;
+	}
+}*/
