50e679b871582d6476014281cc629b7c75c1570a
Goro_sb71@stud.msk.itstep.org <Goro_sb71@stud.msk.itstep.org>
2021-12-10 14:59:33 +0300

    Создан первый класс

--- исключения/Source.cpp
+++ исключения/Source.cpp
@@ -8,6 +8,44 @@ using namespace std;
 присутствовать классы для обработки всевозможных 
 математических ошибок, нехватки памяти, проблемам 
 по работе с файлами и т.д.*/
+class YM {
+	int one;
+	int two;
+public:
+	YM(int a, int b) : one(a), two(b) {}
+		
+	 void as (){
+		int z = one * two;
+		if (one > 0 && two < 0 || one < 0 && two > 0)
+		throw - z;
+	}
+	friend ostream& operator << (ostream& out, const YM& p) {
+		out << p.one << " " << p.two << endl;
+		return out;
+	 }
+	friend istream& operator >> (istream& in,  YM& p) {
+		in >> p.one;
+		in >> p.two;
+		return in;
+	}
+};
+class rabot {
+	
+public:
+	
+};
 int main() {
+	YM one(-5, 5);
+	try {
+		one.as();
+	}
+	catch (int) {
+		cout << "You entered the wrong number" << endl;
+		cin >> one;
+	}
+	//cin >> one;
+	cout << one;
 
+	cout << "proga otrabotala" << endl;
+	
 }
